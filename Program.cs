9a845777d41a5b4e42351b1d3b51bd3990b30fee
Patrick Orellana <Computosala42022@ugb.edu.sv>
2022-07-29 11:59:12 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace App4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduzca su nombre:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Ingrese su edad");
+            int edad = int.Parse(Console.ReadLine());
+            Console.WriteLine("Su nombre es: {1} y tiene {0} años de edad", edad, name);
+        }
+    }
+}
